3292ec8fbbe668f6f97c88f2b670f69e0eb7de1c
Aishwarya Singh <aishsingh@users.noreply.github.com>
2024-08-15 11:04:34 +1000

    Update solution.cpp
    Updated MinCommonValue solution which passes all test cases and is more efficient

--- MinimumCommonValue/solution.cpp
+++ MinimumCommonValue/solution.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
+
+        // two pointer approach to avoid nested for loop
+        int i=0;
+        int j=0;
+
+        while (i < nums1.size() && j < nums2.size())
+        {
+            if (nums1.at(i) > nums2.at(j))
+            {
+                j++;
+            }
+            else if (nums1.at(i) < nums2.at(j))
+            {
+                i++;
+            }
+            else
+            {
+                // nums1[i] and nums2[j] are common
+                return nums1.at(i);
+            }
+        }
+
+
+        /* inefficient code:
+
         for (int i=0; i<nums1.size(); i++)
         {
             for (int j=0; j<nums2.size(); j++)
@@ -11,6 +36,7 @@ public:
                 }
             }
         }
+        */
 
         // no common number found
         return -1;
