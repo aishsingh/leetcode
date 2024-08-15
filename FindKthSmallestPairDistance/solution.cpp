6b7cd85817649712a4884bd219c49d2858ab1525
Aishwarya Singh <aishsingh@users.noreply.github.com>
2024-08-15 21:39:12 +1000

    Update solution.cpp to pass all unit tests

--- FindKthSmallestPairDistance/solution.cpp
+++ FindKthSmallestPairDistance/solution.cpp
@@ -1,26 +1,36 @@
+// Inspired by https://leetcode.com/problems/find-k-th-smallest-pair-distance/solutions/5632765/o-n-log-n-n-log-w-binary-search-sliding-window-java-c-python-go-rust-javascript
+
 #include <algorithm>
 
 class Solution {
 public:
     int smallestDistancePair(vector<int>& nums, int k) {
-        vector<int> distancePairs;
-      
-        // Find all distance pairs 
-        for (int i=0; i<nums.size(); i++)
-        {
-            for (int j=i+1; j<nums.size(); j++)
-            {
-                int distance = abs(nums.at(i) - nums.at(j));
+        
+        sort(nums.begin(), nums.end());
 
-                distancePairs.push_back(distance);
+        int minDistance = 0;
+        int maxDistance = nums.back() - nums.front();
+
+        while (minDistance < maxDistance) {
+            int midDistance = minDistance + (maxDistance - minDistance) / 2;
+            if (countPairsWithinDistance(nums, midDistance) < k) {
+                minDistance = midDistance + 1;
+            } else {
+                maxDistance = midDistance;
             }
         }
+        
+        return minDistance;
+    }
 
-        // Sort pairs
-        if (distancePairs.size() > 1)
-           sort(distancePairs.begin(), distancePairs.end());
-
-        // Find k^th smallest distance pair
-        return distancePairs.at(k-1);
+    int countPairsWithinDistance(const std::vector<int>& numbers, int targetDistance) {
+        int count = 0, left = 0;
+        for (int right = 1; right < numbers.size(); ++right) {
+            while (numbers[right] - numbers[left] > targetDistance) {
+                ++left;
+            }
+            count += right - left;
+        }
+        return count;
     }
 };
