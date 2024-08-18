d7bcfb2c550256a6ded8b1d89506ad7d0705c332
Aishwarya Singh <aishsingh@users.noreply.github.com>
2024-08-18 16:29:09 +1000

    Created AddTwoNumbers solution

---
+++ AddTwoNumbers/solution.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+        ListNode* dummyHead = new ListNode();
+        ListNode* tail = dummyHead;    // track last node in result list
+        bool carryOverOne = false;
+
+        while (l1 != nullptr || l2 != nullptr || carryOverOne)
+        {
+            int digit1 = (l1 != nullptr) ? l1->val : 0;
+            int digit2 = (l2 != nullptr) ? l2->val : 0;
+
+            int sum = digit1 + digit2 + ((carryOverOne) ? 1 : 0);
+            int digit = sum % 10;
+            carryOverOne = sum / 10;
+
+            ListNode* sumNode = new ListNode(digit);
+            tail->next = sumNode;
+            tail = tail->next;
+
+            l1 = (l1 != nullptr) ? l1->next : nullptr;
+            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        }
+
+        ListNode* result = dummyHead->next;
+        delete dummyHead;
+        return result;
+    }
+};
