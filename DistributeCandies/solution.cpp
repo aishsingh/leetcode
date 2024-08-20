fd084a9bf236beb0efc485d227660ecca96b9c14
Aishwarya Singh <aishsingh@users.noreply.github.com>
2024-08-20 16:35:34 +1000

    Updated DistributeCandies solution for a better runtime efficiency

--- DistributeCandies/solution.cpp
+++ DistributeCandies/solution.cpp
@@ -1,19 +1,8 @@
-#include <algorithm>    // std::min
-
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        unordered_set<int> uniqueCandies;
-        
-        // loop through all candies list to find unique types
-        for (int i=0; i<candyType.size(); i++)
-        {
-            if (uniqueCandies.count(candyType.at(i)) == 0)
-            {
-                // New candy type found
-                uniqueCandies.insert(candyType.at(i));
-            }
-        }
+        // Use set to list to the unique types
+        unordered_set<int> uniqueCandies(candyType.begin(), candyType.end());
         
         // calc max candies Alice can eat
         const size_t docCandyLimit = candyType.size() / 2;
