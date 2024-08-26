4ee8d853f2eda08d6b9bd5715c1050f682551842
Aishwarya Singh <mail@aish.codes>
2024-08-26 23:12:46 +1000

    Added RomanToInteger solution passing all test cases

---
+++ RomanToInteger/solution.cpp
@@ -0,0 +1,44 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    int romanToInt(string s) 
+    {
+        // Create hash map for quick roman to value conversions
+        unordered_map<char, int> map = 
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int sum = 0;
+
+        // Iterate over each roman numeral converting to decimal values and adding to the sum
+        for (int i=0; i<s.size(); i++)
+        {
+            bool    isLastCharInStr = (i == s.size()-1);
+
+            // if last char in string there is no next value so set equal to current value to skip to the usual use case 
+            int     currentValue    = map[s.at(i)];
+            int     nextValue       = (!isLastCharInStr) ? map[s.at(i+1)] : currentValue;   
+
+            if (currentValue < nextValue)
+            {
+                // One of the size instances where subtraction is needed for roman to value conversion
+                sum -= currentValue;
+            }
+            else
+            {
+                // Usual use case where roman numerals are written largest to smallest
+                sum += currentValue;
+            }
+        }
+        
+        return sum;
+    }
+};
