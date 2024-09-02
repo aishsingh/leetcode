bfb806abeb4db7daced27466c32fd5ffaade8182
Aishwarya Singh <mail@aish.codes>
2024-09-02 21:09:18 +1000

    Update solution.cpp as all test cases pass now

--- FindTheStudentThatWillReplaceTheChalk/solution.cpp
+++ FindTheStudentThatWillReplaceTheChalk/solution.cpp
@@ -4,9 +4,23 @@ public:
     {
         int student = 0;
 
+        // Handle edge case where there is only one student present
         if (chalk.size() <= 1)
             return student;
 
+        // Handle edge case where there is a large k value
+        // Only running the accumulate fn when the vector size isnt too big to ensure there is no integer overflow
+        if (chalk.size() <= 1000)
+        {
+            // chalk used for a full iteration cycle of the vector
+            int chalkUsedPerCycle   = accumulate(chalk.begin(), chalk.end(), 0);
+            int cyclesNeeded        = k/chalkUsedPerCycle;
+
+            if (cyclesNeeded > 0)
+            {
+                k -= chalkUsedPerCycle * cyclesNeeded;
+            }
+        }
 
         for (;;)
         {
