13de6412ae3a7d3e6c26b186c5a6cb21f03864d2
Aishwarya Singh <mail@aish.codes>
2024-09-02 15:25:40 +1000

    Update AddBinary for working solution

--- AddBinary/solution.cpp
+++ AddBinary/solution.cpp
@@ -5,26 +5,16 @@ public:
 
         int carry = 0;
         int i=0;
-        while (i < a.size()-1 || i < b.size()-1 || carry > 0)
+        while (i < a.size() || i < b.size() || carry > 0)
         {
-            int num1 = (i < a.size()) ? a.at(i) - '0' : 0;
-            int num2 = (i < b.size()) ? b.at(i) - '0' : 0;
+            int num1 = (int(a.size())-1-i >= 0) ? a.at(a.size()-1-i) - '0' : 0;
+            int num2 = (int(b.size())-1-i >= 0) ? b.at(b.size()-1-i) - '0' : 0;
 
-            int binSum = num1 + num2;
-            if (binSum == 1)
-            {
-                binSum = 0;
-                carry++;
-                //carry--; keep the same as it should get incremented too
+            int binSum = num1 + num2 + carry;
+            
+            sum.insert(0, to_string(binSum & 0b01));
 
-
-            }
-            else if (binSum > 1)
-            {
-                  
-            }
-
-            sum.insert(0, to_string(binSum));
+            carry = (binSum & 0b10) >> 1;
             i++;
         }
 
