f431349f2b1bd10d328d09f7bfb39396488d0409
Aishwarya Singh <mail@aishsingh.com>
2024-09-15 13:19:40 +1000

    Create solution.js

---
+++ Debounce/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timeoutRef;
+
+    return function(...args) {
+        clearTimeout(timeoutRef);
+        timeoutRef = setTimeout(() => fn.apply(this, args), t);
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
