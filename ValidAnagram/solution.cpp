3d600b9f94d0a3bf1d8d6cbbb0aad8a9e098cf94
Aishwarya Singh <mail@aishsingh.com>
2024-09-18 09:20:21 +1000

    Improved performance of valid anagram solution

--- ValidAnagram/solution.cpp
+++ ValidAnagram/solution.cpp
@@ -5,16 +5,21 @@ public:
     bool isAnagram(string s, string t) 
     {
         // Use hashmap to keep count of letters in both strings
-        unordered_map<char, int> s1LetterCount;
-        unordered_map<char, int> s2LetterCount;
+        unordered_map<char, int> letterCount;
 
         for (auto letter : s)
-            s1LetterCount[letter]++;
+            letterCount[letter]++;
 
         for (auto letter : t)
-            s2LetterCount[letter]++;
+            letterCount[letter]--;
 
-        // The letter counts in each hashmap should match for the strings to be a valid anagram
-        return (s1LetterCount == s2LetterCount);
+        // To be a valid anagram the letter counts in the hashmap should be all zeros if the count was the same in both strings
+        for (const auto& [letter, count]: letterCount)
+        {
+            if (count != 0)
+                return false;
+        }
+
+        return true;
     }
 };
